82f2b78b13494784250911ce30890c6618900a9c
radude89 <radu.ionut.dan@gmail.com>
2024-08-20 05:31:35 +0300

    Create CRUD operations for MyNotes DB

---
+++ my_notes_app/lib/models/note_database.dart
@@ -0,0 +1,40 @@
+import 'package:isar/isar.dart';
+import 'package:my_notes_app/models/note.dart';
+import 'package:path_provider/path_provider.dart';
+
+class NoteDatabase {
+  static late Isar isar;
+
+  static Future<void> initialize() async {
+    final dir = await getApplicationDocumentsDirectory();
+    isar = await Isar.open([NoteSchema], directory: dir.path);
+  }
+
+  final List<Note> currentNotes = [];
+
+  Future<void> addNote(String text) async {
+    final newNote = Note()..text = text;
+    await isar.writeTxn(() => isar.notes.put(newNote));
+    fetchNotes();
+  }
+
+  Future<void> fetchNotes() async {
+    List<Note> notes = await isar.notes.where().findAll();
+    currentNotes.clear();
+    currentNotes.addAll(notes);
+  }
+
+  Future<void> updateNote(int id, String newText) async {
+    final existingNote = await isar.notes.get(id);
+    if (existingNote != null) {
+      existingNote.text = newText;
+      await isar.writeTxn(() => isar.notes.put(existingNote));
+      await fetchNotes();
+    }
+  }
+
+  Future<void> deleteNote(int id) async {
+    await isar.writeTxn(() => isar.notes.delete(id));
+    await fetchNotes();
+  }
+}
