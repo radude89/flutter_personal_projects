30b6816fb51b0cb975da902cdbc542bb1a997eb1
radude89 <radu.ionut.dan@gmail.com>
2024-08-29 05:40:40 +0300

    Done a bunch of fixes and havea first version of Weather App Complete Weather App

---
+++ weather_app/lib/pages/weather_page.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'package:weather_app/service/weather_service.dart';
+
+import '../models/weather_model.dart';
+
+class WeatherPage extends StatefulWidget {
+  const WeatherPage({super.key});
+
+  @override
+  State<WeatherPage> createState() => _WeatherPageState();
+}
+
+class _WeatherPageState extends State<WeatherPage> {
+
+  final _weatherService = WeatherService('e379cb54c2440b5c542fe44bcd9fa373');
+  Weather? _weather;
+
+  _fetchWeather() async {
+    String cityName = _weatherService.getMockedCity();
+
+    try {
+      final weather = await _weatherService.getWeather(cityName);
+      setState(() {
+        _weather = weather;
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  String getWeatherAnimation(String? mainCondition) {
+    if (mainCondition == null) return 'assets/sunny.json';
+    switch (mainCondition.toLowerCase()) {
+      case 'clouds':
+      case 'mist':
+      case 'smoke':
+      case 'dust':
+      case 'haze':
+      case 'fog':
+        return 'assets/cloudy.json';
+      case 'rain':
+      case 'drizzle':
+      case 'shower rain':
+        return 'assets/rainy.json';
+      case 'thunderstorm':
+        return 'assets/storm.json';
+      case 'clear':
+        return 'assets/sunny.json';
+      default:
+        return 'assets/sunny.json';
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchWeather();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(_weather?.cityName ?? "Loading city..."),
+
+            Lottie.asset(getWeatherAnimation(_weather?.mainCondition)),
+
+            Text('${_weather?.temperature.round().toString() ?? 'Loading temperature...'}°C'),
+
+            Text(_weather?.mainCondition ?? "")
+          ],
+        ),
+      ),
+    );
+  }
+}
