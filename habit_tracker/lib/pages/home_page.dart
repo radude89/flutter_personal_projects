b716f8d0c652a9587d85ee883dbeeca8f334f714
radude89 <radu.ionut.dan@gmail.com>
2024-09-10 05:29:10 +0300

    Add my habit tile

--- habit_tracker/lib/pages/home_page.dart
+++ habit_tracker/lib/pages/home_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:habit_tracker/components/my_drawer.dart';
+import 'package:habit_tracker/components/my_habit_tile.dart';
 import 'package:habit_tracker/database/habit_database.dart';
 import 'package:habit_tracker/models/habit.dart';
+import 'package:isar/isar.dart';
 import 'package:provider/provider.dart';
 
 import '../util/habit_util.dart';
@@ -25,7 +27,10 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        elevation: 0,
+        foregroundColor: Theme.of(context).colorScheme.tertiary,
+      ),
       drawer: const MyDrawer(),
       floatingActionButton: FloatingActionButton(
         onPressed: createNewHabit,
@@ -60,6 +65,60 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
+  void checkHabitOnOff(bool? value, Habit habit) {
+    if (value != null) {
+      context
+          .read<HabitDatabase>()
+          .updateHabitCompletion(habit.id, value);
+    }
+  }
+
+  void editHabitBox(Habit habit) {
+    textController.text = habit.name;
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        content: TextField(
+          controller: textController,
+        ),
+        actions: [
+          MaterialButton(
+            onPressed: () {
+              onUpdate(habit.id);
+            },
+            child: const Text('Save'),
+          ),
+          MaterialButton(
+            onPressed: onCancel,
+            child: const Text('Cancel'),
+          ),
+        ],
+      )
+    );
+  }
+
+  void deleteHabitBox(Habit habit) {
+    textController.text = habit.name;
+    showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: const Text("Are you sure you want to delete?"),
+          actions: [
+            MaterialButton(
+              onPressed: () {
+                onDelete(habit.id);
+              },
+              child: const Text('Delete'),
+            ),
+            MaterialButton(
+              onPressed: onCancel,
+              child: const Text('Cancel'),
+            ),
+          ],
+        )
+    );
+  }
+
   void onSave() {
     String newHabitName = textController.text;
     context.read<HabitDatabase>().addHabit(newHabitName);
@@ -67,7 +126,20 @@ class _HomePageState extends State<HomePage> {
     textController.clear();
   }
 
+  void onUpdate(Id habitID) {
+    String newHabitName = textController.text;
+    context.read<HabitDatabase>().updateHabitName(habitID, newHabitName);
+    Navigator.pop(context);
+    textController.clear();
+  }
+
   void onCancel() {
+    Navigator.pop(context);
+    textController.clear();
+  }
+
+  void onDelete(Id habitID) {
+    context.read<HabitDatabase>().deleteHabit(habitID);
     Navigator.pop(context);
     textController.clear();
   }
@@ -79,9 +151,15 @@ class _HomePageState extends State<HomePage> {
       itemCount: currentHabits.length,
       itemBuilder: (context, index) {
         final habit = currentHabits[index];
-        bool isCompletedToday = isHabitCompletedToday(habit.completedDays);
-        return ListTile(
-          title: Text(habit.name),
+        bool isCompletedToday = isHabitCompletedToday(
+          habit.completedDays
+        );
+        return MyHabitTile(
+          text: habit.name,
+          isCompleted: isCompletedToday,
+          onChanged: (value) => checkHabitOnOff(value, habit),
+          editHabit: (context) => editHabitBox(habit),
+          deleteHabit: (context) => deleteHabitBox(habit),
         );
       }
     );
