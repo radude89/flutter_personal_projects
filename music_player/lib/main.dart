4897c2ce6197adef7f8c36a960f720926e6d4a5e
radude89 <radu.ionut.dan@gmail.com>
2024-10-22 05:24:27 +0300

    Playing songs

--- music_player/lib/main.dart
+++ music_player/lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: const HomePage(),
       theme: Provider
           .of<ThemeProvider>(context)
           .themeData,
--- music_player/lib/models/playlist_provider.dart
+++ music_player/lib/models/playlist_provider.dart
@@ -1,3 +1,4 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 import 'package:music_player/models/song.dart';
 
@@ -7,28 +8,112 @@ class PlaylistProvider extends ChangeNotifier {
       name: "Lose yourself",
       artist: "Eminem",
       albumArtImagePath: "assets/images/a1.png",
-      audioPath: "assets/audio/lose_yourself.mp3"
+      audioPath: "audio/lose_yourself.mp3"
     ),
     Song(
         name: "Not afraid",
         artist: "Eminem",
         albumArtImagePath: "assets/images/a2.png",
-        audioPath: "assets/audio/lose_yourself.mp3"
+        audioPath: "audio/lose_yourself.mp3"
     ),
     Song(
         name: "Stan",
         artist: "Eminem",
         albumArtImagePath: "assets/images/a3.png",
-        audioPath: "assets/audio/lose_yourself.mp3"
+        audioPath: "audio/lose_yourself.mp3"
     )
   ];
 
+  // Audio player
+  final AudioPlayer _audioPlayer = AudioPlayer();
+  Duration _currentDuration = Duration.zero;
+  Duration _totalDuration = Duration.zero;
+  bool _isPlaying = false;
   int? _currentSongIndex;
+
   List<Song> get playlist => _playlist;
   int? get currentSongIndex => _currentSongIndex;
+  bool get isPlaying => _isPlaying;
+  Duration get currentDuration => _currentDuration;
+  Duration get totalDuration => _totalDuration;
 
   set currentSongIndex(int? newIndex) {
     _currentSongIndex = newIndex;
+    if (newIndex != null) {
+      play();
+    }
     notifyListeners();
+  }
+
+  PlaylistProvider() {
+    listenToDuration();
+  }
+
+  void play() async {
+    final String path = _playlist[_currentSongIndex!].audioPath;
+    await _audioPlayer.stop();
+    await _audioPlayer.play(AssetSource(path));
+    _isPlaying = true;
+    notifyListeners();
+  }
+
+  void pause() async {
+    await _audioPlayer.pause();
+    _isPlaying = false;
+    notifyListeners();
+  }
+
+  void resume() async {
+    await _audioPlayer.resume();
+    _isPlaying = true;
+    notifyListeners();
+  }
+
+  void pauseOrResume() async {
+    if (_isPlaying) {
+      pause();
+    } else {
+      resume();
+    }
+    notifyListeners();
+  }
+
+  void seek(Duration position) async {
+    await _audioPlayer.seek(position);
+  }
+
+  void playNextSong() {
+    if (_currentSongIndex != null) {
+      if (_currentSongIndex! < playlist.length - 1) {
+        currentSongIndex = _currentSongIndex! + 1;
+      } else {
+        currentSongIndex = 0;
+      }
+    }
+  }
+
+  void playPreviousSong() async {
+    if (_currentDuration.inSeconds <= 2) { return; }
+    if (_currentSongIndex! > 0) {
+      currentSongIndex = _currentSongIndex! - 1;
+    } else {
+      currentSongIndex = playlist.length - 1;
+    }
+  }
+
+  void listenToDuration() {
+    _audioPlayer.onDurationChanged.listen((newDuration) {
+      _totalDuration = newDuration;
+      notifyListeners();
+    });
+
+    _audioPlayer.onPositionChanged.listen((newPosition) {
+      _currentDuration = newPosition;
+      notifyListeners();
+    });
+
+    _audioPlayer.onPlayerComplete.listen((event) {
+      playNextSong();
+    });
   }
 }
